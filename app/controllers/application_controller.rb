4bbf745cfa33e3643947966fa6bbaa46c0fd1b67
EC2 Default User <ec2-user@ip-172-31-95-235.ec2.internal>
2020-10-21 02:53:41 +0000

    firstcommit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+
+
+  # ログイン後の画面
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admins_top_path(resource.id)
+    when Costomer
+      costomers_top_path(resource.id)
+    end
+  end
+
+  # # ログアウト後の画面
+  # def after_sign_out_path_for(resource_or_scope)
+  #   case resource
+  #   when Admin
+  #     new_admin_session_path
+  #   when Costomer
+  #     new_costomer_session_path
+  #   end
+  # end
+
 end
