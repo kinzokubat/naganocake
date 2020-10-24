77ac4f130f8a797b834793d6f9ac2cf4fb05a6e0
EC2 Default User <ec2-user@ip-172-31-64-135.ec2.internal>
2020-10-24 03:23:19 +0000

    注文

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,10 @@ class Address < ApplicationRecord
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :name, presence: true
+
+
+  def street
+    self.postal_code + self.address + self.name
+  end
+
 end
